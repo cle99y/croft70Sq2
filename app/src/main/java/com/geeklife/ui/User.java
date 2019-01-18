08c7ba7c187421271a4f2a36a9674abfa06a75aa
PaulClegg <chilemerlot@gmail.com>
2019-01-18 14:46:03 +0000

    changes to User Class and layout

--- app/src/main/java/com/geeklife/ui/User.java
+++ app/src/main/java/com/geeklife/ui/User.java
@@ -1,5 +1,6 @@
 package com.geeklife.ui;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.geeklife.croft70squadron.R;
 
@@ -17,7 +19,10 @@ public class User extends Fragment {
     // constant declarations
     final static Boolean USER_EXISTS = true;
     final static Boolean USER_NOT_SET = false;
-    
+    final static Boolean NO_ERRORS = false;
+    final static Boolean HAS_ERRORS = true;
+    final static String EMPTY = null;
+
     View v;
     Button btn;
     TextView header, message, fName, lName, rank;
@@ -33,7 +38,10 @@ public class User extends Fragment {
     public View onCreateView( final LayoutInflater inflater, final ViewGroup container,
                               Bundle savedInstanceState ) {
 
-        isUserSet = false;
+        userInfo = getContext().getSharedPreferences( "USER", Context.MODE_PRIVATE );
+        userEditor = userInfo.edit();
+
+        isUserSet = userInfo.contains( "first_name" );
 
         v = inflater.inflate( R.layout.set_user, container, false );
         btn = v.findViewById( R.id.btn_modify_user );
@@ -50,10 +58,32 @@ public class User extends Fragment {
 
 
         btn.setOnClickListener( new View.OnClickListener() {
+
+
             @Override
             public void onClick( View view ) {
 
-                toggleVisibility();
+                Boolean isError = false;
+
+                if ( isUserSet == USER_NOT_SET ) {
+                    String fn = newFName.getText().toString();
+                    String ln = newLName.getText().toString();
+                    String r = newRank.getText().toString();
+
+                    userEditor.putString( "first_name", fn );
+                    userEditor.putString( "last_name", ln );
+                    userEditor.putString( "rank", r );
+                    userEditor.commit();
+                    Toast.makeText( getContext(),
+                            "Hello " + r + " " + fn + " " + ln,
+                            Toast.LENGTH_LONG ).show();
+                }
+
+
+                if ( isError == NO_ERRORS ) {
+                    isUserSet = !isUserSet;
+                    setForm( isUserSet );
+                }
 
 
             }
@@ -70,6 +100,9 @@ public class User extends Fragment {
                 newUser[i].setVisibility( View.GONE );
                 btn.setText( R.string.modify );
             }
+            fName.setText( userInfo.getString( "first_name", EMPTY ) );
+            lName.setText( userInfo.getString( "last_name", EMPTY ) );
+            rank.setText( userInfo.getString( "rank", EMPTY ) );
         } else {
             for ( int i = 0; i < newUser.length; i++ ) {
                 newUser[i].setVisibility( View.VISIBLE );
